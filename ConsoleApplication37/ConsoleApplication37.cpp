0842c54f0326fc6177d8ded76108060c911d0671
Александр Александр <stripalol@inbox.ru>
2024-02-21 13:05:41 +0700

    Добавьте файлы проекта.

---
+++ ConsoleApplication37/ConsoleApplication37.cpp
@@ -0,0 +1,41 @@
+﻿#include <iostream>
+#include "DEFINE.h"
+using namespace std;
+int main()
+{
+	system("chcp 1251");
+	tare_mult(true)
+	{
+		Arata "\n";
+		Arata "1. кто больше ";
+		Arata "\n";
+		Arata "2. минус";
+		Arata "\n";
+		Arata "3. плюс";
+		Arata "\n";
+		int x;
+		introduce x;
+		int a, b;
+		daca_oriceva(x egal 1)
+		{
+			Arata"введите первое число\n";
+			introduce a;
+			Arata"введите второе число\n";
+			introduce b;
+			Arata "максимум: " << care_max(a, b);
+			Arata "\n";
+		}
+		caz_contar_daca_oriceva(x egal 2)
+		{
+			introduce a;
+			introduce b;
+			Arata "вычитание: "<< minus(a, b);
+		}
+		caz_contar_daca_oriceva(x egal 3)
+		{
+			introduce a;
+			introduce b;
+			Arata "сложение: "<<plus(a, b);
+		}
+	}
+}
---
+++ ConsoleApplication37/DEFINE.h
@@ -0,0 +1,16 @@
+#pragma once
+#define Arata cout<<
+#define KVADRAT(x) (x)*(x)
+#define care_max(x,y) (x>y?x:y)
+#define tare_mult while
+#define daca_oriceva if
+#define introduce cin>>
+#define egal ==
+#define minus(x,y) (x-y)
+#define caz_contar_daca_oriceva else if
+#define plus(x,y) (x+y)
+class DEFINE
+{
+
+};
+
